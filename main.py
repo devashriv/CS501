2012489d1c6a601759ac4940d44c189a74e6dc73
Hang Li <hli@altanova.io>
2021-10-01 16:44:49 -0400

    fix bug

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from preprocessing import *
 
+
 def main():
 
-	train, tweet_raw, test, s = read_data()
-	tweet_split = tweet_split(tweet_raw)
-	tweet_split_lower = tweet_lower(tweet_split)
-	tweet_processed = tweet_stop_words(tweet_split_lower)
-	# tweet_processed = tweet_typo(tweet_processed)
-	
+    train, tweet_raw, test, s = read_data()
+    tweet_processed = tweet_split(tweet_raw)
+    tweet_processed = tweet_lower(tweet_processed)
+    tweet_processed = tweet_stop_words(tweet_processed)
+    # tweet_processed = tweet_typo(tweet_processed)
+
     print(train)
 
 

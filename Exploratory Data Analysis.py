031ccc9d34a58a545daea9da4dc6d57da45b38f5
jessizzz <91638180+jessizzz@users.noreply.github.com>
2021-10-02 22:24:20 -0400

    EDA - 1
    Basic EDA, next step N-gram

--- Exploratory Data Analysis.py
+++ Exploratory Data Analysis.py
@@ -11,67 +11,38 @@ import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 from textblob import TextBlob
 
-
-# In[2]:
-
-
 traindata = pd.read_csv("train.csv", index_col = [0])
 
 
-# In[3]:
-
-
 traindata.head()
-
-
-# In[4]:
 
 
 traindata.info() # have 7613 text and 7552 keyword, maybe removed the text that does not providing keyword?
                  # But the text without location is huge number, maybe just leave it as this? 
 
 
-# In[5]:
-
-
-sns.heatmap(traindata.isnull(),yticklabels = False, cbar = False, cmap = "viridis")
-
-
-# In[6]:
+sns.heatmap(traindata.isnull(),yticklabels = False, cbar = False, cmap = "viridis") # heatmap to see null in each coloumn, keyword has few, loacation has too many
 
 
 df_train = pd.DataFrame(traindata)
 
 
-# In[7]:
-
-
 df_train["target"].value_counts() # 1 means real disaster and 0 means fake ones. More real ones than fake ones
 
 
-# In[8]:
-
-
-df_train["keyword"].value_counts() # most 'popular' word here is fatalities, and we have about 221 different
+df_train["keyword"].value_counts() # to see the frequency of each keyword
+                                   # most 'popular' word here is fatalities, and we have about 221 different
                                    # words, each with different frequency. We want to look more than just keyword, 
                                    # but could start with keywords
 
 
-# In[9]:
-
-
-df_train["length"] = df_train["text"].apply(len)
+df_train["length"] = df_train["text"].apply(len) # add 'length' column in df
 df_train.head()
 
-
-# In[10]:
 
 
 df_train.hist(column = "length", by = "target", bins = 50, figsize = (20,10)) # rough idea of the distribution 
                                                                               # of twitter length for 0 and 1
-
-
-# In[11]:
 
 
 df_train['target_count'] = df_train.groupby('keyword')['target'].transform('mean')
@@ -86,29 +57,6 @@ sns.countplot(y = df_train.sort_values(by='target_count', ascending = False)["ke
 # This is directly from the competition coding script, I think it's really nice to have, but may need to write 
 # it with different style
 
-
-# In[18]:
-
-
-corpus = open("train.csv").read()
-ngram_object = TextBlob(corpus)
-gram_2 = ngram_object.ngrams(n = 2)
-gram_3 = ngram_object.ngrams(n = 3)
-
-
-# In[19]:
-
-
-print(gram_2[:10])
-
-
-# In[20]:
-
-
-print(gram_3[:10])
-
-
-# In[ ]:
 
 
 
